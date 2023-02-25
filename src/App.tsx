b97944d2664aa61ac522a81105e77040df39543b
Mikhail-Martynenko <misha.martynenko8@yandex.ru>
2023-02-25 17:58:23 +0300

    added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,26 @@ import './scss/app.scss'
 import MainPage from "./pages/MainPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import {Route, Routes} from "react-router-dom";
-import CartPage from "./pages/CartPage";
-import ItemPage from "./pages/ItemPage/ItemPage";
 import StaticLayout from "./components/StaticLayout";
+import React, {Suspense} from "react";
 
+const CartPage = React.lazy(() => import(/*webpackChunkName: "CartPage"*/'./pages/CartPage'));
+const ItemPage = React.lazy(() => import(/*webpackChunkName: "ItemPage"*/'./pages/ItemPage/ItemPage'));
 function App() {
     return (
         <Routes>
             <Route path="/" element={<StaticLayout/>}>
                 <Route path='apple-store' element={<MainPage/>}/>
-                <Route path='apple-store/cart' element={<CartPage/>}/>
-                <Route path='apple-store/item/:id' element={<ItemPage/>}/>
+                <Route path='apple-store/cart' element={
+                    <Suspense fallback={<div>Загрузка...</div>}>
+                        <CartPage/>
+                    </Suspense>
+                }/>
+                <Route path='apple-store/item/:id' element={
+                    <Suspense fallback={<div>Загрузка...</div>}>
+                        <ItemPage/>
+                    </Suspense>
+                }/>
                 <Route path='*' element={<NotFoundPage/>}/>
             </Route>
         </Routes>
